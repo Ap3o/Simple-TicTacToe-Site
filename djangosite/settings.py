0ef38ab8f6deaa64fdb9f2026bccc720f517bd01
NeoD <flesakov.nik09@gmail.com>
2020-02-07 22:04:17 +0500

    Change redirect url

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Редирект уже залогинненых пользователей
+LOGIN_REDIRECT_URL = '/'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
